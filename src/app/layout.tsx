51e56be9480589b082ac90f105147e95fda8a910
ignarru <arruvitoignacio@gmail.com>
2026-05-07 09:29:47 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,19 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Google Analytics 4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H2DYQH0VWY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H2DYQH0VWY');
+          `}
+        </Script>
       </head>
       <body className={`${geistSans.variable} antialiased`}>{children}</body>
     </html>
